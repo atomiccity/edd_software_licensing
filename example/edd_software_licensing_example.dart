1db137ce8f8e43168850a9b9d8565bf6ecdaa0e1
Michael R. Taylor <michael@atomic.city>
2025-01-12 17:01:24 -0600

    Updated the example code

--- example/edd_software_licensing_example.dart
+++ example/edd_software_licensing_example.dart
@@ -1,6 +1,66 @@
 import 'package:edd_software_licensing/edd_software_licensing.dart';
 
-void main() {
-  // var awesome = Awesome();
-  // print('awesome: ${awesome.isAwesome}');
+void main() async {
+  var eddLicensingClient = EddSoftwareLicensingClient(licenseHost: 'google.com');
+
+  // Activate a license
+  var activateResponse = await eddLicensingClient.activateLicense(
+    itemId: 1,
+    licenseKey: 'MYLICENSEKEY',
+  );
+  if (activateResponse.isSuccessful()) {
+    // License was activated
+  } else {
+    // Figure out what went wrong
+    if (activateResponse.serverResponseCode != 200) {
+      // We got a HTTP server error
+    } else {
+      // We got a licensing error
+      var licenseStatus = activateResponse.license;
+      var errorMessage = activateResponse.error;
+    }
+  }
+
+  // Check the activation
+  var checkResponse = await eddLicensingClient.checkLicense(
+    itemId: 1,
+    licenseKey: 'MYLICENSEKEY',
+  );
+  if (checkResponse.isSuccessful()) {
+    // License is good
+  } else {
+    // Figure out what went wrong
+    if (activateResponse.serverResponseCode != 200) {
+      // We got a HTTP server error
+    } else {
+      // We got a licensing error
+      var licenseStatus = activateResponse.license;
+      var errorMessage = activateResponse.error;
+    }
+  }
+
+  // Deactivate the license
+  var deactivateResponse = await eddLicensingClient.deactivateLicense(
+    itemId: 1,
+    licenseKey: 'MYLICENSEKEY',
+  );
+  if (checkResponse.isSuccessful()) {
+    // License is deactivated
+  } else {
+    // Figure out what went wrong
+    if (activateResponse.serverResponseCode != 200) {
+      // We got a HTTP server error
+    } else {
+      // We got a licensing error
+      var licenseStatus = activateResponse.license;
+      var errorMessage = activateResponse.error;
+    }
+  }
+
+  // Get latest version of software
+  var versionResponse = await eddLicensingClient.getVersion(itemId: 1);
+  if (versionResponse.isSuccessful()) {
+    var newestVersion = versionResponse.newVersion;
+    var stableVersion = versionResponse.stableVersion;
+  }
 }
